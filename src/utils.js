9e689aa6bd6ef506f8727930d6d91f1d6f783947
Diego Lopes <diegolopes087@gmail.com>
2024-06-12 12:09:44 -0300

    round kbytes to integer

--- src/utils.js
+++ src/utils.js
@@ -4,7 +4,13 @@ export const formatBytes = (bytes) => {
   const sizes = ["B", "KB", "MB", "GB", "TB"];
   const i = Math.floor(Math.log(bytes) / Math.log(1024));
   const size = bytes / Math.pow(1024, i);
-  const roundedSize = Math.round(size * 100) / 100;
 
-  return `${roundedSize} ${sizes[i]}`;
+  let formattedSize;
+  if (sizes[i] === "KB") {
+    formattedSize = Math.floor(size);
+  } else {
+    formattedSize = Math.round(size * 100) / 100; //
+  }
+
+  return `${formattedSize} ${sizes[i]}`;
 };
