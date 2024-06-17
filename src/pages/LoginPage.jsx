0548443e989297711149d8481fccbc41522ef87a
Diego Lopes <diegolopes087@gmail.com>
2024-06-17 16:56:27 -0300

    Login page refactor

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,10 +2,9 @@ import React, { useEffect } from "react";
 import { GoogleLogin } from "@react-oauth/google";
 import { useNavigate } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
-import Logo from "../assets/logo.svg";
 import { toast } from "react-toastify";
 import localforage from "localforage";
-import Icon from "../components/Icon";
+import Logo from "../assets/logo.svg";
 
 const LoginPage = () => {
   const navigate = useNavigate();
@@ -45,26 +44,17 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="flex h-screen">
-      <div className="flex flex-col flex-grow gap-8 justify-center items-center bg-coolBlue">
-        <div className="flex items-center gap-4 text-white">
-          <h1 className="font-sans text-6xl font-bold">filebox</h1>
-          <Icon className="text-7xl" src={"box.svg"} />
-        </div>
-        <img src={Logo} alt="Logo" className="w-1/3 max-w-xs" />
-      </div>
-      <div className="flex flex-col flex-1 gap-8 justify-center items-center bg-white p-8">
-        <h1 className="text-2xl font-semibold text-gray-700">
-          Entre com sua conta do Google
-        </h1>
-        <GoogleLogin
-          onSuccess={handleSuccess}
-          onError={handleError}
-          theme="outline"
-          shape="pill"
-          scope={["profile", "email"]}
-        />
-      </div>
+    <div className=" h-screen font-sans font-bold  bg-coolBlue text-white flex flex-col justify-center items-center">
+      <h1 className="text-7xl">filebox</h1>
+      <img src={Logo} className=" size-96" />
+      <h1 className="text-md mt-4 ">Entre com sua conta do Google</h1>
+      <GoogleLogin
+        onSuccess={handleSuccess}
+        onError={handleError}
+        theme="outline"
+        shape="pill"
+        scope={["profile", "email"]}
+      />
     </div>
   );
 };
